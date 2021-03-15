ca63ce4e964ecf38d83e42cceaf77a5d644148e9
qwe661234 <qwe661234@gmail.com>
2021-03-16 01:24:45 +0800

    copy in 64 bit version

--- question3/bitcopy.c
+++ question3/bitcopy.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "bitcopy.h"
 
 int main(int _argc, char **_argv)
 {
-    memset(&input[0], 0xFF, sizeof(input));
+    clock_t start, end;
+    start = clock();
+    memset(&input, 0xFF, sizeof(input));
 
     for (int i = 1; i <= 32; ++i) {
         for (int j = 0; j < 16; ++j) {
             for (int k = 0; k < 16; ++k) {
-                memset(&output[0], 0x00, sizeof(output));
+                memset(&output, 0x00, sizeof(output));
                 printf("%2d:%2d:%2d ", i, k, j);
-                bitcpy(&output[0], k, &input[0], j, i);
-                dump_binary(&output[0], 8);
+                bitcpy(&output, k, &input, j, i);
+                dump_64bits(output);
                 printf("\n");
             }
         }
     }
 
+    end = clock();
+    end -= start;
+    printf("%ld ms\n", end);
     return 0;
 }
--- question3/bitcopy.h
+++ question3/bitcopy.h
@@ -7,75 +7,24 @@ void bitcpy(void *_dest,      /* Address of the buffer to write to */
             size_t _read,     /* Bit offset to start reading from */
             size_t count)
 {
-    size_t read_lhs = _read & 7;
-    size_t read_rhs = 8 - read_lhs;
-    const uint8_t *source = (const uint8_t *) _src + (_read / 8);
-    size_t write_lhs = _write & 7;
-    size_t write_rhs = 8 - write_lhs;
-    uint8_t *dest = (uint8_t *) _dest + (_write / 8);
-
-    static const uint8_t read_mask[] = {
-        0x00, /*    == 0    00000000b   */
-        0x80, /*    == 1    10000000b   */
-        0xC0, /*    == 2    11000000b   */
-        0xE0, /*    == 3    11100000b   */
-        0xF0, /*    == 4    11110000b   */
-        0xF8, /*    == 5    11111000b   */
-        0xFC, /*    == 6    11111100b   */
-        0xFE, /*    == 7    11111110b   */
-        0xFF  /*    == 8    11111111b   */
-    };
-
-    static const uint8_t write_mask[] = {
-        0xFF, /*    == 0    11111111b   */
-        0x7F, /*    == 1    01111111b   */
-        0x3F, /*    == 2    00111111b   */
-        0x1F, /*    == 3    00011111b   */
-        0x0F, /*    == 4    00001111b   */
-        0x07, /*    == 5    00000111b   */
-        0x03, /*    == 6    00000011b   */
-        0x01, /*    == 7    00000001b   */
-        0x00  /*    == 8    00000000b   */
-    };
-
-    while (count > 0) {
-        uint8_t data = *source++;
-        // how many bytes you want to write this round
-        size_t bitsize = (count > 8) ? 8 : count;
-        if (read_lhs > 0) {
-            data <<= read_lhs;
-            if (bitsize > read_rhs)
-                // put the context in next bytes to data               
-                data |= (*source >> read_rhs);
-        }
-        // keep the number of bit in data you want to write
-        if (bitsize < 8)
-            data &= read_mask[bitsize];
-
-        uint8_t original = *dest;
-        uint8_t mask = read_mask[write_lhs];
-        if (bitsize > write_rhs) {
-            /* Cross multiple bytes */
-            // write first bytes
-            *dest++ = (original & mask) | (data >> write_lhs);
-            // keep the context of next bytes
-            original = *dest & write_mask[bitsize - write_rhs];
-            // write next bytes
-            *dest = original | (data << write_rhs);
-        } else {
-            // Since write_lhs + bitsize is never >= 8, no out-of-bound access.
-            
-            // keep context you do not write from tail
-            mask |= write_mask[write_lhs + bitsize];
-            // write bytes
-            *dest++ = (original & mask) | (data >> write_lhs);
-        }
-
-        count -= bitsize;
+    uint64_t data;
+    const uint64_t *source = _src;
+    uint64_t *dest = _dest;
+    data = *source;
+    data = data << _read;
+    // data >> _read + (63 - (_read + count) + 1)
+    data = data >> (64 - count);
+    data = data << (64 - (_read + count));
+    if(_read > _write){
+        data = data << (_read - _write);
+    }else{
+        data = data >> (_write - _read);
     }
+    *dest |= data;
 }
 
-static uint8_t output[8], input[8];
+// static uint8_t output[8], input[8];
+static uint64_t output, input;
 
 static inline void dump_8bits(uint8_t _data)
 {   
@@ -87,4 +36,10 @@ static inline void dump_binary(uint8_t *_buffer, size_t _length)
 {   
     for (int i = 0; i < _length; ++i)
         dump_8bits(*_buffer++);
+}
+
+static inline void dump_64bits(uint64_t _data)
+{   
+    for (int i = 0; i < 64; ++i)
+        printf("%d", (_data & (0x8000000000000000 >> i)) ? 1 : 0);
 }
