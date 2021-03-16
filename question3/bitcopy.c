db0c595e01c9b2b505b3a02ebc38ec43174e862a
qwe661234 <qwe661234@gmail.com>
2021-03-16 10:09:13 +0800

    Add test data

--- question3/bitcopy.c
+++ question3/bitcopy.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 #include "bitcopy.h"
 
 int main(int _argc, char **_argv)
 {
-    clock_t start, end;
-    start = clock();
+    FILE *fp = fopen("b.txt", "w");
+    if (!fp) {
+        perror("failed to open b.txt");
+        exit(EXIT_FAILURE);
+    }
     memset(&input[0], 0xFF, sizeof(input));
 
     for (int i = 1; i <= 32; ++i) {
         for (int j = 0; j < 16; ++j) {
             for (int k = 0; k < 16; ++k) {
+                clock_t start, end;
+                start = clock();
                 memset(&output[0], 0x00, sizeof(output));
                 printf("%2d:%2d:%2d ", i, k, j);
                 bitcpy(&output[0], k, &input[0], j, i);
                 dump_binary(&output[0], 8);
                 printf("\n");
+                end = clock();
+                end -= start;
+                fprintf(fp, "%ld ms\n", end);
             }
         }
     }
-    end = clock();
-    end -= start;
-    printf("%ld ms\n", end);
     return 0;
 }
