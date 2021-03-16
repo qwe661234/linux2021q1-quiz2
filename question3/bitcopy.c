0d85ab6ff26441779ba6e6b53ed9fb640aebfc32
qwe661234 <qwe661234@gmail.com>
2021-03-16 10:44:33 +0800

    Add test data of 64 bit copy

--- question3/bitcopy.c
+++ question3/bitcopy.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 #include "bitcopy.h"
 
 int main(int _argc, char **_argv)
 {
-    clock_t start, end;
-    start = clock();
-    memset(&input, 0xFF, sizeof(input));
-
+    FILE *fp = fopen("a.txt", "w");
+    if (!fp) {
+        perror("failed to open a.txt");
+        exit(EXIT_FAILURE);
+    }
+    memset(&input[0], 0xFF, sizeof(input));
     for (int i = 1; i <= 32; ++i) {
         for (int j = 0; j < 16; ++j) {
             for (int k = 0; k < 16; ++k) {
-                memset(&output, 0x00, sizeof(output));
+                clock_t start, end;
+                start = clock();
+                memset(&output[0], 0x00, sizeof(output));
                 printf("%2d:%2d:%2d ", i, k, j);
-                bitcpy(&output, k, &input, j, i);
-                dump_64bits(output);
+                bitcpy(&output[0], k, &input[0], j, i);
                 printf("\n");
+                end = clock();
+                end -= start;
+                fprintf(fp, "%d\n", i);
             }
         }
     }
-
-    end = clock();
-    end -= start;
-    printf("%ld ms\n", end);
     return 0;
 }
--- question3/bitcopy.h
+++ question3/bitcopy.h
@@ -1,30 +1,7 @@
 #include <stdint.h>
 
-/*read from src and write to dest*/
-void bitcpy(void *_dest,      /* Address of the buffer to write to */
-            size_t _write,    /* Bit offset to start writing to */
-            const void *_src, /* Address of the buffer to read from */
-            size_t _read,     /* Bit offset to start reading from */
-            size_t count)
-{
-    uint64_t data;
-    const uint64_t *source = _src;
-    uint64_t *dest = _dest;
-    data = *source;
-    data = data << _read;
-    // data >> _read + (63 - (_read + count) + 1)
-    data = data >> (64 - count);
-    data = data << (64 - (_read + count));
-    if(_read > _write){
-        data = data << (_read - _write);
-    }else{
-        data = data >> (_write - _read);
-    }
-    *dest |= data;
-}
-
-// static uint8_t output[8], input[8];
-static uint64_t output, input;
+static uint8_t output[8], input[8];
+// static uint64_t output, input;
 
 static inline void dump_8bits(uint8_t _data)
 {   
@@ -42,4 +19,28 @@ static inline void dump_64bits(uint64_t _data)
 {   
     for (int i = 0; i < 64; ++i)
         printf("%d", (_data & (0x8000000000000000 >> i)) ? 1 : 0);
+}
+
+/*read from src and write to dest*/
+void bitcpy(void *_dest,      /* Address of the buffer to write to */
+            size_t _write,    /* Bit offset to start writing to */
+            const void *_src, /* Address of the buffer to read from */
+            size_t _read,     /* Bit offset to start reading from */
+            size_t count)
+{
+    uint64_t data;
+    const uint64_t *source = (const uint64_t *)_src;
+    uint64_t *dest = (uint64_t *)_dest;
+    data = *source;
+    data = data << _read;
+    // data >> _read + (63 - (_read + count) + 1)
+    data = data >> (64 - count);
+    data = data << (64 - (_read + count));
+    if(_read > _write){
+        data = data << (_read - _write);
+    }else{
+        data = data >> (_write - _read);
+    }
+    *dest |= data;
+    dump_64bits(*dest);
 }
